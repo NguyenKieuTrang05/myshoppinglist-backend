9ad0bf2f3a4c9443af2de0dc81ad7ce8c7d39d2a
NguyenKieuTrang05 <kieutrngnguyen@gmail.com>
2026-04-13 12:56:18 +0200

    generated spring project

---
+++ src/main/java/de/htw_berlin/myshoppinglist/MyshoppinglistApplication.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.myshoppinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshoppinglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshoppinglistApplication.class, args);
+	}
+
+}
